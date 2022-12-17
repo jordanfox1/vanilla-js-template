98183115661dccd653e0b70d0680647fe05e733a
Jordan Fox <jordan.fox@canstar.com.au>
2022-12-17 16:02:52 +1000

    home/about page bones set up

--- scripts/about/about.js
+++ scripts/about/about.js
@@ -1,8 +1,12 @@
-
+import { appendHtmlToBody, addElement } from "../helpers"
 
 export const showabout = async () => {
-    const body = await fetch('../../routing/pages/about.html')
-    const response = await body.text()
-    document.getElementById("content").innerHTML = response
-};
+    // globals 
+    const pageName = 'About' 
+
+
+    // set up page body
+    await appendHtmlToBody('../../routing/pages/about.html')
+    addElement({type: 'h1', text: `hello and welcome to the ${pageName} page --- from js`}, 'content');
+}
 
